c20d77bdaf46a33f131d8a796fbf86a1dad6a4cb
Sangmi Lee <qdpnok@naver.com>
2026-04-01 15:51:02 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/portsang/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/portsang/portfolio/domain/repository/ProjectRepository.kt
@@ -2,8 +2,14 @@ package com.portsang.portfolio.domain.repository
 
 import com.portsang.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.Optional
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
+
+    @Query("select p from Project p left join fetch p.details where p.id = :id")
+    override fun findById(id: Long) : Optional<Project>
 }
