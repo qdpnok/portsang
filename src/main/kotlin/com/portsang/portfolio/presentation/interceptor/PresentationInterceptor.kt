942ae2c09f9f5e22480fbf7ab2666643ec9ed4c2
Sangmi Lee <qdpnok@naver.com>
2026-04-12 19:57:51 +0900

    인터셉터 개발

--- src/main/kotlin/com/portsang/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/portsang/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.portsang.portfolio.presentation.interceptor
 
+import com.portsang.portfolio.domain.entity.HttpInterface
+import com.portsang.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Configuration
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
--- src/main/kotlin/com/portsang/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/portsang/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -1,7 +1,17 @@
 package com.portsang.portfolio.presentation.interceptor
 
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Component
-class PresentationInterceptorConfiguration {
+class PresentationInterceptorConfiguration(
+  private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/admin/**", "h2**",
+                "/favicon.ico", "/error")
+    }
 }
