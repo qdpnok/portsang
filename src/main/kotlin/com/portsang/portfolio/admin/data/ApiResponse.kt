b2ad5066058739a94f46e4357361aba899ebe030
Sangmi Lee <qdpnok@naver.com>
2026-04-18 18:11:41 +0900

    공통 개발 - DTO

--- src/main/kotlin/com/portsang/portfolio/admin/data/ApiResponse.kt
+++ src/main/kotlin/com/portsang/portfolio/admin/data/ApiResponse.kt
@@ -4,4 +4,17 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 class ApiResponse<T> (status: HttpStatus) : ResponseEntity<T>(status) {
+    companion object {
+        fun successCreate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 저장되었습니다.")
+        }
+
+        fun successUpdate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 수정되었습니다.")
+        }
+
+        fun successDelete(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 삭재되었습니다.")
+        }
+    }
 }
--- src/main/kotlin/com/portsang/portfolio/admin/data/TableDTO.kt
+++ src/main/kotlin/com/portsang/portfolio/admin/data/TableDTO.kt
@@ -1,11 +1,43 @@
 package com.portsang.portfolio.admin.data
 
-import javax.swing.table.TableColumn
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
 
 class TableDTO(
     val name: String,
     val columns: List<String>,
     val records: List<List<String>>
 ) {
+    companion object {
+        fun<T : Any> form(classInfo: KClass<T>, entities: List<Any>, vararg filterings: String): TableDTO {
+            // ["a", "b"]
+            // "a", "b"
+            val name = classInfo.simpleName ?: "Unknown"
+            val columns = createColumns(classInfo, *filterings)
+            val records = entities.map { entity ->
+                columns.map { columns ->
+                    classInfo.memberProperties
+                        .find { columns.equals(it.name) }
+                        ?.getter
+                        ?.call(entity)
+                        .toString()
+                }.toList()
+            }.toList()
 
+            return TableDTO(name = name, columns = columns, records = records)
+        }
+
+        private fun<T: Any> createColumns(classInfo: KClass<T>, vararg filterings: String): MutableList<String> {
+            val mainColumns = classInfo.java.declaredFields
+                .filter { !filterings.contains(it.name) }
+                .map { it.name }
+                .toMutableList()
+
+            val baseColumns = classInfo.java.superclass.declaredFields
+                .map { it.name }
+                .toMutableList()
+
+            return (mainColumns + baseColumns).toMutableList()
+        }
+    }
 }
