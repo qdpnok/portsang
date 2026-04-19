f841798227193fd72d87b70b5558767a1e0d7047
Sangmi Lee <qdpnok@naver.com>
2026-04-19 21:01:09 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 일대다

---
+++ src/main/kotlin/com/portsang/portfolio/admin/context/project/controller/AdminProjectApiController.kt
@@ -0,0 +1,41 @@
+package com.portsang.portfolio.admin.context.project.controller
+
+import com.portsang.portfolio.admin.context.project.form.ProjectForm
+import com.portsang.portfolio.admin.context.project.service.AdminProjectService
+import com.portsang.portfolio.admin.data.ApiResponse
+import com.portsang.portfolio.admin.data.TableDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/admin/api/project")
+class AdminProjectApiController(
+    private val adminProjectService: AdminProjectService
+) {
+
+    @PostMapping
+    fun postProject(@RequestBody @Validated form: ProjectForm): ResponseEntity<Any> {
+        adminProjectService.save(form)
+
+        return ApiResponse.successCreate()
+    }
+
+    @PutMapping("/{id}")
+    fun putProject(@PathVariable id: Long, @RequestBody form: ProjectForm): ResponseEntity<Any> {
+        adminProjectService.update(id, form)
+
+        return ApiResponse.successUpdate()
+    }
+
+    @GetMapping("/{id}/details")
+    fun getProjectDetails(@PathVariable id: Long): TableDTO {
+        return adminProjectService.getProjectDetailTable(id)
+    }
+}
---
+++ src/main/kotlin/com/portsang/portfolio/admin/context/project/form/ProjectDetailForm.kt
@@ -0,0 +1,23 @@
+package com.portsang.portfolio.admin.context.project.form
+
+import com.portsang.portfolio.domain.entity.ProjectDetail
+import jakarta.validation.constraints.NotBlank
+
+data class ProjectDetailForm(
+    val id: Long,
+
+    @field:NotBlank(message = "필수값입니다.")
+    val content : String,
+    
+    val url : String?,
+
+    val isActive: Boolean,
+) {
+    fun toEntity() : ProjectDetail {
+        return ProjectDetail(
+            content = this.content,
+            url = this.url,
+            isActive = this.isActive
+        )
+    }
+}
